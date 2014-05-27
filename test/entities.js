fc20befee85ae8a479ffcb98290968ac82c11d93
ajlopez <webmaster@ajlopez.com>
2014-05-27 15:26:48 -0300

    Get entities

--- test/entities.js
+++ test/entities.js
@@ -3,11 +3,28 @@ var apps = require('../lib/applications');
 
 var app = apps.createApplication('Warehouse');
 
+exports['Get no entity'] = function (test) {
+    var entities = app.getEntities();
+    
+    test.ok(entities);
+    test.ok(Array.isArray(entities));
+    test.equal(entities.length, 0);
+};
+
 exports['Create entity'] = function (test) {
     var entity = app.createEntity('Product');
     
     test.ok(entity);
     test.equal(entity.getTitle(), 'Product');
+};
+
+exports['Get one entity'] = function (test) {
+    var entities = app.getEntities();
+    
+    test.ok(entities);
+    test.ok(Array.isArray(entities));
+    test.equal(entities.length, 1);
+    test.equal(entities[0].getTitle(), 'Product');
 };
 
 
