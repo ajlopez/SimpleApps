6b6d07ebf290f696f9a7838703123d3155441973
ajlopez <webmaster@ajlopez.com>
2014-05-28 15:56:25 -0300

    First properties in entity tests

--- test/entities.js
+++ test/entities.js
@@ -11,6 +11,12 @@ exports['Get no entity'] = function (test) {
     test.equal(entities.length, 0);
 };
 
+exports['Get no entity by title'] = function (test) {
+    var entity = app.getEntityByTitle('Foo');
+    
+    test.equal(entity, null);
+};
+
 exports['Create entity'] = function (test) {
     var entity = app.createEntity('Product');
     
@@ -27,4 +33,28 @@ exports['Get one entity'] = function (test) {
     test.equal(entities[0].getTitle(), 'Product');
 };
 
+exports['Create entity and add property'] = function (test) {
+    var entity = app.createEntity('Category');
+    
+    test.ok(entity);
+    
+    var property = entity.createProperty('Name');
+    
+    test.ok(property);
+    test.equal(property.getTitle(), 'Name');
+};
 
+exports['Get entity by title'] = function (test) {
+    var entity = app.getEntityByTitle('Category');
+    
+    test.ok(entity);
+    test.equal(entity.getTitle(), 'Category');
+    
+    var properties = entity.getProperties();
+    
+    test.ok(properties);
+    test.ok(Array.isArray(properties));
+    test.equal(properties.length, 1);
+    
+    test.equal(properties[0].getTitle(), 'Name');
+};
