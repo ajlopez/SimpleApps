80491e958b215021949b309638c1f423eb22c4a1
ajlopez <webmaster@ajlopez.com>
2014-05-29 17:23:50 -0300

    Get applications, first tests

--- test/applications.js
+++ test/applications.js
@@ -1,6 +1,14 @@
 
 var apps = require('../lib/applications');
 
+exports['Get No Applications'] = function (test) {
+    var applications = apps.getApplications();
+    
+    test.ok(applications);
+    test.ok(Array.isArray(applications));
+    test.equal(applications.length, 0);
+};
+
 exports['Create application'] = function (test) {
     var app = apps.createApplication('Warehouse');
     
@@ -8,3 +16,11 @@ exports['Create application'] = function (test) {
     test.equal(app.getTitle(), 'Warehouse');
 };
 
+exports['Get One Application'] = function (test) {
+    var applications = apps.getApplications();
+    
+    test.ok(applications);
+    test.ok(Array.isArray(applications));
+    test.equal(applications.length, 1);
+    test.equal(applications[0].getTitle(), 'Warehouse');
+};
